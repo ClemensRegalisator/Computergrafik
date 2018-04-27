9b76c3d946788958a959b6b709a7f825d513474b
Clemens <clemens.klein@student-hsrm.de>
2018-04-27 17:05:08 +0200

    Oida es geht

--- Raytracer/Main.py
+++ Raytracer/Main.py
@@ -1,7 +1,7 @@
 from Raytracer.Geo import Point
 from Raytracer.Geo import Vector
 from Raytracer.Ray import Ray
-from Raytracer.Plane import Plane
+from Raytracer.Plane import *
 from Raytracer.Triangle import Triangle
 from Raytracer.Sphere import Sphere
 from PIL import Image
@@ -10,11 +10,13 @@ import time
 
 #initialisierung
     #bild
-imageHeight = 400
-imageWidth = 400
+REFLECTION = 0.3
+MAX_LEVEL = 10
+imageHeight = 1080
+imageWidth = 1920
 wRes = 5
 hRes = 5
-BACKGROUND_COLOR = (255, 255, 255) #konstante für hintergrundfarbe
+BACKGROUND_COLOR = Vector(0, 0, 0) #konstante für hintergrundfarbe
 SCHATTIG = (0, 0, 0)
 fieldOfView = math.pi / 8   #öffnungswinkel
 aspectRatio = imageWidth / imageHeight
@@ -24,15 +26,15 @@ width = aspectRatio * height
 pixelWidth = width / (imageWidth - 1)
 pixelHeight = height / (imageHeight - 1)
 
-img = Image.new('RGB', (imageWidth, imageHeight), BACKGROUND_COLOR)
+img = Image.new('RGB', (imageWidth, imageHeight), (BACKGROUND_COLOR.x, BACKGROUND_COLOR.y, BACKGROUND_COLOR.z))
 
     #objekte
 s1 = Sphere(Point(40, 0, 1), 2, (0, 255, 0))
 s2 = Sphere(Point(40, 3, -2), 2, (255, 0, 0))
 s3 = Sphere(Point(40, -3, -2), 2, (0, 0, 255))
 t1 = Triangle(Point(40, 3, -2), Point(40, -3, -2), Point(40, 0, 1), (255, 255, 0))
-e1 = Plane(Point(0, 0, -7), Vector(0, 0, 1), (0, 128, 128))
-objectlist = [e1, s1, s2, s3]            #liste der objekte auf die gestrahlt wird
+e1 = Plane(Point(0, 0, -7), Vector(0, 0, 1), (0, 255, 0), True)
+objectlist = [e1, s1, s2, s3, t1]            #liste der objekte auf die gestrahlt wird
 
     #kamera
 e = Point(0, 0, 0)   #augenposition
@@ -44,6 +46,7 @@ f = ce / ce.length()
 fxup = f.cross(up)
 s = fxup / fxup.length()
 u = s.cross(f)
+
 
 #lichtfarben
 ca = Vector(50, 50,50)
@@ -76,15 +79,58 @@ def phong(schnittpunkt, ray, object):
     n = (object.normalAt(schnittpunkt)).normalized()
     lr = (lv.cross(n)).normalized()
     d = ray.direction.normalized()
-    cin = Vector(object.colorAt(ray)[0], object.colorAt(ray)[1], object.colorAt(ray)[2])
+    if type(object) == Plane:
+        cin = Vector(object.colorAt(schnittpunkt)[0], object.colorAt(schnittpunkt)[1], object.colorAt(schnittpunkt)[2])
+    else:
+        cin = Vector(object.colorAt(ray)[0], object.colorAt(ray)[1], object.colorAt(ray)[2])
     amb = ca * ka
     dif = cin * (kd * lv.dot(n))
     spek = cin * (ks * (lr.dot(d * (-1)) ** 2))
     rgb = amb + dif + spek
-    return (int(rgb.x), int(rgb.y), int(rgb.z))
+    if schatten(schnittpunkt):
+        return Vector(int(rgb.x / 1.7), int(rgb.y / 1.7), int(rgb.z / 1.7))
+    return Vector(int(rgb.x), int(rgb.y), int(rgb.z))
+
+
+def intersect(ray, level):
+    if level >= MAX_LEVEL:
+        return None
+    aktObj = None
+    maxdist = float('inf')
+    for object in objectlist:
+        hitdist = object.intersectionParameter(ray)
+        if hitdist:
+            if 0 < hitdist < maxdist:
+                maxdist = hitdist
+                aktObj = object
+    if maxdist == float('inf'):
+        return None
+    else:
+        return (ray.origin + ray.direction * maxdist, aktObj)
+
+
+def computeReflectedRay(hitPointData, ray):
+    n = hitPointData[1].normalAt(hitPointData[0])
+    newRay = (ray.direction - 2 * n.dot(ray.direction) * n)
+    return Ray(hitPointData[0], newRay)
+
+def shade(level, hitPointData, ray):
+
+
+    directColor = phong(hitPointData[0], ray, hitPointData[1])
+    reflectedRay = computeReflectedRay(hitPointData, ray)
+    reflectedColor = traceRay(level + 1, reflectedRay)
 
 
 
+    return directColor + REFLECTION * reflectedColor
+
+
+def traceRay(level, ray):
+    hitPointData = intersect(ray, level)
+    if hitPointData:
+        return shade(level, hitPointData, ray)
+    return BACKGROUND_COLOR
 
 
 def cheese():
@@ -107,21 +153,19 @@ def cheese():
             img.putpixel((imageWidth - 1 - x, imageHeight - 1 - y), color)
         if x % progress == 0:
             print("Fortschritt: " + str((x / imageWidth) * 100) + "%")
-'''
-def intersect(level, ray, maxlevel):
-    return 0
 
+def cheese2():
+    progress = imageWidth / 10
+    for x in range(imageWidth):
+        for y in range(imageHeight):
+            ray = calcRay(x, y)              #noch zu implementieren
+            color = traceRay(0, ray)
+            color = (int(color.x), int(color.y), int(color.z))
+            img.putpixel((imageWidth - 1 - x, imageHeight - 1 - y), color)
+        if x % progress == 0:
+            print("Fortschritt: " + str((x / imageWidth) * 100) + "%")
 
-def traceRay(level, ray):
-    hitpointData = intersect(level, ray, maxlevel)
-
-def shade(level, hitpointData,ray, object):
-    directColor = phong(hitpointData, ray, object)
-
-    reflectedRay = computeReflectedRay(hitpointData)
-    reflectColor = traceRay(level+1, reflectedRay)
-'''
 a = time.time()
-cheese()
+cheese2()
 print(time.time() - a)
 img.show()
--- Raytracer/Plane.py
+++ Raytracer/Plane.py
@@ -1,8 +1,17 @@
+from Raytracer.Geo import *
 class Plane(object):
-    def __init__(self, point, normal, color):
+    def __init__(self, point, normal, color, checker: bool):
         self.point = point #point
         self.normal = normal.normalized() #vector
         self.color = color
+        self.checker = checker
+        if checker:
+            self.baseColor = (255, 255, 255)
+            self.otherColor = (0, 0, 0)
+            self.ambientCoefficient = 1.0
+            self.diffuseCoefficient = 0.6
+            self.specularCoefficient = 0.2
+            self.checkSize = 1
 
     def __repr__(self):
         return 'Plane(%s,%s)' %(repr(self.point), repr(self.normal))
@@ -19,5 +28,14 @@ class Plane(object):
     def normalAt(self, p):
         return self.normal
 
-    def colorAt(self, ray):
+    def colorAt(self, p):
+        if self.checker:
+            v = Vector(p.x, p.y, p.z)
+            v = v * (1.0 / self.checkSize)
+            if (int(abs(v.x) + 0.5) + int(abs(v.y) + 0.5) + int(abs(v.z) + 0.5)) % 2:
+
+                return self.otherColor
+
+            return self.baseColor
         return self.color
+
